b725061237937449b2816ca0294e92d0baa0f08b
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-08 13:36:55 -0700

    button works

--- app.js
+++ app.js
@@ -6,4 +6,11 @@
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
+const result = document.getElementById('random-num');
+const generateButton = document.getElementById('generate');
 
+
+generateButton.addEventListener('click', () => {
+   // console.log('click');
+
+})
